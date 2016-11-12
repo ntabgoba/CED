1ca6d9a5679bfd53eb3a6f282991a6e1e0dccd04
ntabgoba <ntabgoba@gmail.com>
2016-11-12 17:38:38 +0900

    2011 CED mSV

--- MinSci/ced.R
+++ MinSci/ced.R
@@ -10,13 +10,9 @@ library(ggplot2)
 # (1st Vehicle-borne Survey) ( August 2011 )
 # http://emdb.jaea.go.jp/emdb/en/portals/b133/
 air_2011 <- read.csv(file = "10200000002_07.csv", header = TRUE)
-dim(air_2011)
-
 names(air_2011) <- c("gridcode","sdate","edate","pref","city","no_samples","AvgAirDoseRate",
                      "NE_nLat","NE_eLong","NW_nLat","NW_eLong",
                      "SW_nLat","SW_eLong","SE_nLat","SE_eLong")
-air_2011 <- read.csv("air_2011.csv")
-
 air_2011$AnnualExtDose <- (air_2011$AvgAirDoseRate - 0.04)*(16 + 8*0.4)*365/1000
 air_2011$sdate <- as.Date(air_2011$sdate,"%Y-%m-%d")
 air_2011$edate <- as.Date(air_2011$edate,"%Y-%m-%d")
@@ -29,11 +25,6 @@ air_2011$AnnualDoseRange <- cut(air_2011$AnnualExtDose, c(0,1,5,10,20,50,100,250
 areakm2 <- as.data.frame(table(air_2011$AnnualDoseRange))
 areakm2$Var1 <- areakm2$AnnualDoseRange
 
-iro <- colorFactor(
-        palette = "Blues",
-        domain = air_2011$pop_quants
-)
-
 iro2 <- colorFactor(
         palette = "PuRd",
         domain = air_2011$AnnualDoseRange
@@ -42,8 +33,18 @@ air_2011_plot <- leaflet() %>%
         addTiles()%>%
         addRectangles(data = air_2011,lng1 = ~SW_eLong, lat1 = ~SW_nLat,
                       lng2 = ~NE_eLong, lat2 = ~NE_nLat,
-                      color = ~iro2(air_2011$AnnualDoseRange))
+                      color = ~iro2(air_2011$AnnualDoseRange))%>%
+        addLegend("bottomright", pal = iro2, values = air_2011$AnnualDoseRange,
+                  title = "2013 CEDmSv/y",
+                  opacity = 1)%>%
+        addPopups(lat = 37.4211, lng = 141.0328,popup = "FDNPP")
 air_2011_plot
+
+
+
+
+
+
 #Readings of Detailed Monitoring in the Restricted Area and Planned Evacuation Zone 
 # (6th Vehicle-borne Survey) ( From Feburary 2013 to March 2013 )
 air_2013 <- read.csv(file = "10200000007_07.csv", header = TRUE)
@@ -116,10 +117,25 @@ air_2013_plot <- leaflet() %>%
                   opacity = 1)%>%
         addPopups(lat = 37.4211, lng = 141.0328,popup = "FDNPP")
 air_2013_plot
+
+
+
+
+
+
+
 #  Readings of Detailed Monitoring in the Areas to Which Evacuation Orders Have Been Issued 
 # (17th Vehicle-borne Survey) ( From March 2014 to April 2014 )
 air_2014 <- read.csv(file = "10202600017_07.csv", header = TRUE)
 dim(air_2014)
+
+
+
+
+
+
+
+
 
 #MinSci
 # March 2015, Air Dose Rates Measured by Route Buses in Fukushima Prefecture
